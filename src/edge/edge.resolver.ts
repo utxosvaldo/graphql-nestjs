829691c3eb44359a39460402df23d4760b885eb6
utxosvaldo <osvaldo.rosales.96@gmail.com>
2025-07-24 15:35:47 -0600

    feat: add basic edges query to resolve graphql scheme generation

--- src/edge/edge.resolver.ts
+++ src/edge/edge.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+// edge.resolver.ts
+import { Resolver, Query } from '@nestjs/graphql';
+import { Edge } from './edge.entity';
+import { EdgeService } from './edge.service';
 
-@Resolver()
-export class EdgeResolver {}
+@Resolver(() => Edge)
+export class EdgeResolver {
+  constructor(private edgeService: EdgeService) { }
+
+  @Query(() => [Edge], { name: 'edges' })
+  async getEdges(): Promise<Edge[]> {
+    return this.edgeService.findAll();
+  }
+}
--- src/edge/edge.service.ts
+++ src/edge/edge.service.ts
@@ -1,4 +1,17 @@
+// edge.service.ts
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Edge } from './edge.entity';
 
 @Injectable()
-export class EdgeService {}
+export class EdgeService {
+  constructor(
+    @InjectRepository(Edge)
+    private edgeRepository: Repository<Edge>,
+  ) { }
+
+  async findAll(): Promise<Edge[]> {
+    return this.edgeRepository.find();
+  }
+}
