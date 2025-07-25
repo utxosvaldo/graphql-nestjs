75275e936bca409efa96e10a981be16bea55e975
utxosvaldo <osvaldo.rosales.96@gmail.com>
2025-07-24 18:54:54 -0600

    implement emit edge.created event

--- src/edge/edge.module.ts
+++ src/edge/edge.module.ts
@@ -3,9 +3,10 @@ import { EdgeService } from './edge.service';
 import { EdgeResolver } from './edge.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Edge } from './entities/edge.entity';
+import { RabbitmqModule } from 'src/rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Edge])],
+  imports: [TypeOrmModule.forFeature([Edge]), RabbitmqModule],
   providers: [EdgeService, EdgeResolver]
 })
 export class EdgeModule { }
--- src/edge/edge.service.ts
+++ src/edge/edge.service.ts
@@ -1,28 +1,19 @@
 // edge.service.ts
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Edge } from './entities/edge.entity';
 import { CreateEdgeInput } from './dto/create-edge.input';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class EdgeService {
   constructor(
     @InjectRepository(Edge)
     private edgeRepository: Repository<Edge>,
+    @Inject('RABBITMQ_SERVICE')
+    private client: ClientProxy,
   ) { }
-
-  async create(createEdgeInput: CreateEdgeInput): Promise<Edge> {
-    // Generate random capacity between 10,000 and 1,000,000 as per requirements
-    const capacity = Math.floor(Math.random() * (1000000 - 10000 + 1)) + 10000;
-
-    const edge = this.edgeRepository.create({
-      ...createEdgeInput,
-      capacity,
-    });
-
-    return this.edgeRepository.save(edge);
-  }
 
   async findAll(): Promise<Edge[]> {
     return this.edgeRepository.find();
@@ -35,5 +26,24 @@ export class EdgeService {
     }
 
     return edge;
+  }
+
+
+  async create(createEdgeInput: CreateEdgeInput): Promise<Edge> {
+    // Generate random capacity between 10,000 and 1,000,000 as per requirements
+    const capacity = Math.floor(Math.random() * (1000000 - 10000 + 1)) + 10000;
+
+    const edge = this.edgeRepository.create({
+      ...createEdgeInput,
+      capacity,
+    });
+
+    const savedEdge = await this.edgeRepository.save(edge);
+    console.log(`Edge created: ${JSON.stringify(savedEdge, null, 2)}`);
+
+    this.client.emit('edge.created', savedEdge);
+
+
+    return savedEdge;
   }
 }
